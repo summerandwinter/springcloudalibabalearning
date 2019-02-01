820f5f50c4075232be4e159551f1063765bce028
yi.liu <ly1989124423@163.com>
2019-02-01 13:46:12 +0800

    init

---
+++ alibaba-nacos-config-client/src/test/java/com/bmsoft/alibabanacosconfigclient/AlibabaNacosConfigClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bmsoft.alibabanacosconfigclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlibabaNacosConfigClientApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
